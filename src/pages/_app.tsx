0e7ea3e5bebf24d95f82c803f63769cc48933750
Billy Joe Santos <santos16426@gmail.com>
2024-01-02 05:21:03 +0800

    init commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import Layout from '@/app/components/Layout';
+import { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps } : AppProps) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+};
+
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,116 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { cn } from '@/app/lib/utils';
+import { useEffect, useState } from 'react';
+import { Play, Pause, Square, TimerReset } from 'lucide-react';
 
-const inter = Inter({ subsets: ['latin'] })
+type TimerType = {
+    id: number,
+    type: string,
+    value: number
+};
 
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+const timer: TimerType[] = [
+    {
+        id: 0,
+        type: "Pomodoro",
+        value: 1
+    },
+    {
+        id: 1,
+        type: "Short Break",
+        value: 5
+    },
+    {
+        id: 2,
+        type: "Long Break",
+        value: 10
+    },
+];
+const App = () => {
+    const [activeTab, setActiveTab] = useState<number>(0);
+    const [timerValue, setTimerValue] = useState<number>(25 * 60); // Initial value in seconds
+    const [isRunning, setIsRunning] = useState<boolean>(false);
+
+    const tabs: string[] = [
+        "Pomodoro",
+        "Short Break",
+        "Long Break"
+    ];
+
+    
+
+    useEffect(() => {
+        setTimerValue(timer[activeTab].value * 60); 
+    }, [activeTab]);
+
+    useEffect(() => {
+        let timerInterval: NodeJS.Timeout;
+
+        if (isRunning && timerValue > 0) {
+            timerInterval = setInterval(() => {
+                setTimerValue(prevValue => prevValue - 1);
+            }, 1000);
+            
+        }
+        if(timerValue === 0){
+            alert("finish timer")
+        }
+
+        return () => clearInterval(timerInterval);
+    }, [isRunning, timerValue]);
+
+    const handleStartPause = () => {
+        setIsRunning(prevValue => !prevValue);
+    };
+
+    const handleReset = () => {
+        setTimerValue(timer[activeTab].value * 60);
+        setIsRunning(false);
+    };
+
+    return (
+        <div className={cn('h-screen flex flex-col justify-center items-center gap-5')}>
+            <div className='bg-white p-5 rounded-2xl bg-opacity-40 w-[500px] shadow-lg'>
+                <div className='flex flex-row gap-4 px-2 justify-around'>
+                {tabs.map((tab, index) => (
+                    <div
+                        key={index}
+                        onClick={() => setActiveTab(index)}
+                        className={cn(
+                            'py-2 px-4 rounded-md cursor-pointer transition duration-300',
+                            activeTab === index
+                                ? 'bg-teal-500 text-white font-bold shadow-lg'
+                                : 'bg-teal-600 text-white hover:bg-teal-500'
+                        )}
+                    >
+                        {tab}
+                    </div>
+                ))}
+                </div>
+                <div className='text-[10rem] text-center font-bold text-white'>{Math.floor(timerValue / 60)}:{timerValue % 60 < 10 ? '0' : ''}{timerValue % 60}</div>
+                <div className="flex gap-4 flex-row justify-center mt-4">
+                    <button
+                        className={cn('text-white py-2 px-4 rounded-md transition duration-300  flex flex-col justify-center  items-center',
+                            isRunning ? 'hover:bg-orange-600 bg-orange-400' : 'hover:bg-green-600 bg-green-400',
+                        )}
+                        onClick={handleStartPause}
+                    >
+                        {isRunning ? <Pause size={20} /> : <Play size={20} />}
+                        {isRunning? 'Pause' : 'Play'}
+                    </button>
+                    <button
+                        className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 transition duration-300 flex flex-col justify-center  items-center"
+                        onClick={handleReset}
+                    >
+                        <TimerReset size={20}/> Reset
+                    </button>
+                </div>
+            </div>
+
+            <div className='bg-white p-5 rounded-2xl bg-opacity-40 w-[500px] shadow-lg'>
+                Todo list
+            </div>
         </div>
-      </div>
+    );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default App;
